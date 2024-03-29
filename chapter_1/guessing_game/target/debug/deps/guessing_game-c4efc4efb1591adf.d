968cc17de488842889f4c1a5c1d5971b3b5557c3
Tommaso Di Marcello <t.dimarcello@gmail.com>
2024-03-29 21:02:32 +0000

    guessing game

---
+++ chapter_1/guessing_game/target/debug/deps/guessing_game-c4efc4efb1591adf.d
@@ -0,0 +1,5 @@
+/Users/admin/Developer/rust/chapter_1/guessing_game/target/debug/deps/guessing_game-c4efc4efb1591adf: src/main.rs
+
+/Users/admin/Developer/rust/chapter_1/guessing_game/target/debug/deps/guessing_game-c4efc4efb1591adf.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_1/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/admin/Developer/rust/chapter_1/guessing_game/target/debug/guessing_game: /Users/admin/Developer/rust/chapter_1/guessing_game/src/main.rs
