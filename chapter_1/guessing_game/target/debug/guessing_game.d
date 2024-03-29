e62fb9735c596c8504fa6c71986e446c3662b3f8
Tommaso Di Marcello <t.dimarcello@gmail.com>
2024-03-29 21:06:34 +0000

    Stop tracking target directories

--- chapter_1/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/admin/Developer/rust/chapter_1/guessing_game/target/debug/guessing_game: /Users/admin/Developer/rust/chapter_1/guessing_game/src/main.rs
--- chapter_1/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/admin/Developer/rust/chapter_1/hello_cargo/target/debug/hello_cargo: /Users/admin/Developer/rust/chapter_1/hello_cargo/src/main.rs
